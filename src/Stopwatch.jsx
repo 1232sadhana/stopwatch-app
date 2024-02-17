59df952385b5be96b09676fd21d13e3823761bbe
CrioUser <crio-user@criodo.com>
2024-02-17 15:47:57 +0530

    Test commit

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -8,7 +8,7 @@ const Stopwatch = () => {
     let timer;
     if (isRunning) {
       timer = setInterval(() => {
-        setTime(prevTime => prevTime + 1);
+        setTime((prevTime) => prevTime + 1);
       }, 1000);
     } else {
       clearInterval(timer);
@@ -32,7 +32,7 @@ const Stopwatch = () => {
   const formatTime = () => {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
-    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
   };
 
   return (
@@ -40,11 +40,9 @@ const Stopwatch = () => {
       <h1>Stopwatch</h1>
       <div className="time">{formatTime()}</div>
       <div className="buttons">
-        {!isRunning ? (
-          <button onClick={startStopwatch}>Start</button>
-        ) : (
-          <button onClick={stopStopwatch}>Stop</button>
-        )}
+        <button onClick={isRunning ? stopStopwatch : startStopwatch}>
+          {isRunning ? 'Stop' : 'Start'}
+        </button>
         <button onClick={resetStopwatch}>Reset</button>
       </div>
     </div>
