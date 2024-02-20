fb6f87105eb258efa1eca5f6c7444d65f88011ec
SADHANA JADHAV <136025180+1232sadhana@users.noreply.github.com>
2024-02-20 18:55:19 +0000

    Test commit

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -8,7 +8,7 @@ const Stopwatch = () => {
     let timer;
     if (isRunning) {
       timer = setInterval(() => {
-        setTime((prevTime) => prevTime + 1);
+        setTime(prevTime => prevTime + 1);
       }, 1000);
     } else {
       clearInterval(timer);
@@ -32,7 +32,7 @@ const Stopwatch = () => {
   const formatTime = () => {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
-    return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
   };
 
   return (
